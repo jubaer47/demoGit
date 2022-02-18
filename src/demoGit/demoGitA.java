d8cea0c8216ce8afe3ddcc15718aba5767ad3ab6
jubaer47 <jubaerhosain47@gmail.com>
2022-02-18 02:56:54 -0500

    my git push to the git repo

---
+++ src/demoGit/demoGitA.java
@@ -0,0 +1,18 @@
+package demoGit;
+
+public class demoGitA {
+	
+	public static void git() {
+		
+	}
+	
+	
+public static void main(String[] args) {
+	demoGitA.git();
+System.out.println("learing git");
+	
+}
+	
+	
+	
+}
